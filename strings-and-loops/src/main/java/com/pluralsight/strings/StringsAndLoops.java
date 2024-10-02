62534b53a8673861884d5f02c436b0258101c83b
Keante Young <youngkeante@gmail.com>
2024-10-02 16:01:02 -0400

    FullNameGenerator

---
+++ strings-and-loops/src/main/java/com/pluralsight/strings/StringsAndLoops.java
@@ -0,0 +1,9 @@
+package com.pluralsight.strings;
+
+import java.sql.SQLOutput;
+
+public class StringsAndLoops {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(s.toLowerCase());
+
