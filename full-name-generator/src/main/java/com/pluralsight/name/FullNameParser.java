057c0e3d517ca2ccdb816fda0b81878933d12c65
Keante Young <youngkeante@gmail.com>
2024-10-03 22:56:11 -0400

    FullNameParser

--- full-name-generator/src/main/java/com/pluralsight/name/FullNameParser.java
+++ full-name-generator/src/main/java/com/pluralsight/name/FullNameParser.java
@@ -4,31 +4,29 @@ import java.util.Scanner;
 
 public class FullNameParser {
 
-        public static void main(String[] args) {
-            String firstName;
-            String middleName;
-            String lastName;
+    public static void main(String[] args) {
+        String firstName;
+        String middleName;
+        String lastName;
 
-            Scanner userInput = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
 
-            System.out.print("Please Enter Your Name (first last or first middle last): ");
-            String input = userInput.nextLine();
-            String[] nameParts = input.split (" ");
+        System.out.print("Please Enter Your Name (first last or first middle last): ");
+        String input = userInput.nextLine();
+        String[] nameParts = input.split (" ");
 
-            System.out.println("Parsed Name Components:");
-            if (nameParts.length == 2) {
-                System.out.println("First Name: " + nameParts[0]);
-                System.out.println("Last Name: " + nameParts[1]);
-            } else if (nameParts.length == 3) {
-                System.out.println("First Name: " + nameParts[0]);
-                System.out.println("Middle Name: " + nameParts[1]);
-                System.out.println("Last Name: " + nameParts[2]);
-            } else {
-                System.out.println("Uh Oh! Please Enter The Name In The Correct Format.");
-            }
-
-            userInput.close();
+        System.out.println("Parsed Name Components:");
+        if (nameParts.length == 2) {
+            System.out.println("First Name: " + nameParts[0]);
+            System.out.println("Last Name: " + nameParts[1]);
+        } else if (nameParts.length == 3) {
+            System.out.println("First Name: " + nameParts[0]);
+            System.out.println("Middle Name: " + nameParts[1]);
+            System.out.println("Last Name: " + nameParts[2]);
+        } else {
+            System.out.println("Uh Oh! Please Enter The Name In The Correct Format.");
         }
+
+        userInput.close();
     }
-
-
+}
